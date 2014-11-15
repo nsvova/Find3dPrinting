33b02f1b4dda68ac2e8bce2c98baccdce4a5ed1b
nsvova <volodymyr.nos@globallogic.com>
2014-11-16 01:36:32 +0200

    Added solution
    Without Controllers

---
+++ Source/Find3dPrinting.Data.Interfaces/IDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Find3dPrinting.Data.Interfaces
+{
+    public interface IDataModel<ModelType>
+    {
+        ModelType GetById(int id);
+        ModelType GetById(int[] id);
+        void Add(ModelType model);
+        void Add(int id);
+        void Remove(ModelType model);
+        void Remove(int id);
+        void Update(int id, ModelType newModel);
+    }
+}
