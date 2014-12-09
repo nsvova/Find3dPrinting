dafa77318ed41d327d1b092a362f259bdbb7b7e8
andrewberezovskyy <andrewberezovskyy@gmail.com>
2014-12-09 20:23:50 +0200

    Added views (Step3,Step4) , controller MakeOrderController

---
+++ Source/Find3dPrinting/Controllers/MakeOrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Find3dPrinting.Controllers
+{
+    public class MakeOrderController : Controller
+    {
+        //
+        // GET: /MakeOrder/
+
+        public ActionResult Step3_ChoosePrintProperties()
+        {
+            return View();
+        }
+
+
+        public ActionResult Step4_FindManufacturers()
+        {
+            return View();
+        }
+
+    }
+}
